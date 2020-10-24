c1664941738f50f53d270dadc926b2a6813d6431
melikeaydogan <melike.aydo99@gmail.com>
2020-10-24 14:37:40 +0300

    created the light version

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
     	PrimeGenerator primeGenerator = ​new​ PrimeGenerator();
     	System.out.​println​(primeGenerator.generatePrimes(​50​));
+    	System.out.println(​"Successfully created light version"​)​;
     }
 }
